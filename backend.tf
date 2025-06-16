acd3f1b654f53a056e298e52285a2bbc0be688ed
Trần Hoài Phú <phubao789@gmail.com>
2025-06-16 17:04:54 +0700

    Update code and fix error backend

---
+++ backend.tf
@@ -0,0 +1,17 @@
+# This is a partial backend configuration.
+# The 'key' attribute will be provided via the command line during initialization.
+#
+# For DEV environment:
+# terraform init -backend-config="key=dev/terraform.tfstate"
+#
+# For PROD environment:
+# terraform init -backend-config="key=prod/terraform.tfstate"
+
+terraform {
+  backend "s3" {
+    bucket         = "mapapp-terraform-state-storage" # Please replace with your unique bucket name
+    region         = "ap-southeast-1"
+    dynamodb_table = "mapapp-terraform-state-lock"
+    encrypt        = true
+  }
+} 
