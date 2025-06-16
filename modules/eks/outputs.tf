e5ae8ab238c24e303e2d3cb493830afc6533e4ed
Trần Hoài Phú <phubao789@gmail.com>
2025-06-16 16:23:42 +0700

    Fix code error terraform

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -6,10 +6,16 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "jenkins_role_arn" {
-  value = aws_iam_role.jenkins_irsa.arn
-}
-
 output "node_security_group_id" {
   value = module.eks.node_security_group_id
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC provider."
+  value       = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider_url" {
+  description = "The URL of the OIDC provider."
+  value       = module.eks.oidc_provider
 }
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,23 +1,31 @@
 variable "cluster_name" {
-  type = string
+  description = "The name of the EKS cluster."
+  type        = string
 }
 
 variable "kubernetes_version" {
-  type = string
+  description = "The desired Kubernetes version for the EKS cluster."
+  type        = string
 }
 
 variable "vpc_id" {
-  type = string
+  description = "The VPC ID where the EKS cluster and its nodes will be deployed."
+  type        = string
 }
 
 variable "private_subnet_ids" {
-  type = list(string)
+  description = "A list of private subnet IDs for the EKS cluster and nodes."
+  type        = list(string)
 }
 
-variable "azs" {
-  type = list(string)
+variable "eks_managed_node_groups" {
+  description = "A map of managed node groups to create."
+  type        = any
+  default     = {}
 }
 
 variable "tags" {
-  type = map(string)
+  description = "A map of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
 }
