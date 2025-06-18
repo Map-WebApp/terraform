08edab4df1a3ff41eaa82a8e098aab5f971cc297
Trần Hoài Phú <phubao789@gmail.com>
2025-06-18 22:35:39 +0700

    Fix: module eks

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -31,3 +31,8 @@ output "cluster_certificate_authority_data" {
   description = "The base64 encoded certificate data required to communicate with the cluster."
   value       = module.eks.cluster_certificate_authority_data
 }
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded cluster CA data."
+  value       = module.eks.cluster_certificate_authority_data
+}
