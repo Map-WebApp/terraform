4a8f13e34c2690a084fe5e7ab6a58cc853bdba0d
Trần Hoài Phú <phubao789@gmail.com>
2025-06-18 22:33:03 +0700

    Fix code error terraform apply

---
+++ providers.tf
@@ -0,0 +1,46 @@
+# Added by terraform_fix: Kubernetes & Helm providers with exec auth to EKS
+# Make sure AWS CLI v2 is installed in the environment running Terraform.
+
+provider "aws" {
+  region = "us-east-1"
+  
+  default_tags {
+    tags = {
+      Environment = "dev"
+      Project     = "mapapp-devsecops"
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+} 
